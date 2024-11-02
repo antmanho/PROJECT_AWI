235fcfee7a71632695017ff855b3d82b9a132df3
BARBEDET ANTHONY <163284599+antmanho@users.noreply.github.com>
2024-11-02 16:35:45 +0100

    maj2

---
+++ FRONT/src/app/creer-session/creer-session.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-creer-session',
+  standalone: true,
+  imports: [],
+  templateUrl: './creer-session.component.html',
+  styleUrl: './creer-session.component.css'
+})
+export class CreerSessionComponent {
+
+}
