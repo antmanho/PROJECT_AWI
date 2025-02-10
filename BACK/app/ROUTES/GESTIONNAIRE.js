2b394475addb4039e1cb69bdda66690ed0f1730f
CorentinDelclaud <corentin.delclaud@gmail.com>
2025-02-10 17:02:10 +0100

    maj gestionnaire back acces db catalogue

--- BACK/app/ROUTES/GESTIONNAIRE.js
+++ BACK/app/ROUTES/GESTIONNAIRE.js
@@ -667,6 +667,7 @@ module.exports = (db) => {
                 Stock.est_en_vente
             FROM Stock
             JOIN Session ON Stock.numero_session_actuelle = Session.id_session
+            WHERE est_en_vente = 1
         `;
         
     
